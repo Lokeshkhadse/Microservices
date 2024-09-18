987ca129bf6afb3b7bbb6d546d59c5b2cb697a05
Lokeshkhadse <99036671+Lokeshkhadse@users.noreply.github.com>
2024-09-18 02:29:51 -0700

    miceroservices

--- miroservices Project Day 2/MICROSERVICES/HotelService/src/main/java/com/example/hotel/service/Repositroy/HotelRespositroy.java
+++
@@ -1,7 +0,0 @@
-package com.example.hotel.service.Repositroy;
-
-import com.example.hotel.service.Entity.Hotel;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface HotelRespositroy extends JpaRepository<Hotel, String> {
-}
--- miroservices Project Day 2/MICROSERVICES/UserService/src/main/java/com/example/user/service/Repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.user.service.Repository;
-
-import com.example.user.service.Entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, String> {
-}
